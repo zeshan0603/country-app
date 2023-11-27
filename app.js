f4d54381ec06cf5582581702e79e282da848e7c0
Jim K <kiousisdimitris8@gmail.com>
2023-11-27 08:58:38 +0200

    added the navbar hamburger menu is not done here

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // WEBSITE URL
 const apiURL = "https://restcountries.com/";
 // TRY TO KEEP THE NAME IN SMALL LETTERS
@@ -13,12 +12,10 @@ const getByCurrencyCode = `https://restcountries.com/v3.1/currency/${currency}`;
 const getByLanguage = `https://restcountries.com/v3.1/lang/${language}`;
 const getByCapital = `https://restcountries.com/v3.1/capital/${capital}`;
 
-
-// // 
+// //
 // fetch(getAllCountries)
 // .then((res) => res.json())
 // .then((data) => console.log(data));
 
-
 // This is the same as the above
-axios.get(getAllCountries).then((res)=> console.log(res.data))
+// axios.get(getAllCountries).then((res) => console.log(res.data));
